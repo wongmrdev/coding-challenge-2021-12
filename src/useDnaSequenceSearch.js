1db8dbda68bc54801c7b920bdc1ae3a79c1a55c0
Matthew Wong <matthew.raymond.wong@gmail.com>
2022-01-04 03:09:30 -1000

    lazy loading

---
+++ src/useDnaSequenceSearch.js
@@ -0,0 +1,46 @@
+import { useEffect, useState, useRef } from 'react'
+import { API, graphqlOperation } from 'aws-amplify'
+import { listDnaSequences } from './graphql/queries'
+
+export default function useDnaSequenceSearch(query, nextToken) {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [dnaSequences, setDnaSequences] = useState([])
+  //const [hasMore, setHasMore] = useState(false)
+  const nextNextToken = useRef(undefined)
+
+  useEffect(() => {
+    setDnaSequences([])
+  }, [query])
+
+  useEffect(() => {
+    setLoading(true)
+    setError(false)
+    const controller = new AbortController();
+    const signal = controller.signal
+    API.graphql(graphqlOperation(listDnaSequences, {
+      filter: { bases: { contains: query } },
+      limit: 5,
+      signal,
+      ...nextToken && { nextToken: nextToken }
+    })).then(dnaSequenceData => {
+      console.log(dnaSequenceData)
+      console.log(dnaSequenceData.data.listDnaSequences.items)
+      setDnaSequences(prevDnaSequences => {
+        return [...new Set([...prevDnaSequences, ...dnaSequenceData.data.listDnaSequences.items])]
+      })
+      if (dnaSequenceData.data.listDnaSequences.nextToken) { nextNextToken.current = dnaSequenceData.data.listDnaSequences.nextToken }
+      console.log(nextNextToken)
+      setLoading(false)
+    }).catch(err => {
+      if (err.name === "AbortError") return
+      setError(true)
+      console.log(err)
+    })
+    return () => {
+      controller.abort()
+    }
+  }, [query, nextToken, nextNextToken])
+
+  return { dnaSequences, nextNextToken, loading, error, }
+}
